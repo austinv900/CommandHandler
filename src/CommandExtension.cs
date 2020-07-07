a7cb135afd9a06512b0a16c502788d1a4fa17bd1
Austin Vandersluis <austinv900@nindroidservers.com>
2020-07-07 08:07:26 -0700

    Added Bind for static interfaces

--- src/CommandExtension.cs
+++ src/CommandExtension.cs
@@ -6,6 +6,18 @@ namespace CommandHandler
 {
     public static class CommandExtension
     {
+        public static void Bind<TCommandInterface>(this ICommandHandler handler)
+        {
+            Type t = typeof(TCommandInterface);
+            MethodInfo[] methods = t.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+
+            for (int i = 0; i < methods.Length; i++)
+            {
+                MethodInfo m = methods[i];
+                handler.Bind(m, null);
+            }
+        }
+
         public static void Bind<TCommandInterface>(this ICommandHandler handler, TCommandInterface instance)
         {
             Type t = instance.GetType();
